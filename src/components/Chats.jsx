18abb2474f8111bcae852dd1b246bd464a0a12a2
Tushar Damahe <qubetushar@gmail.com>
2024-03-27 23:22:29 +0530

    fetching private chat messages between two users

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,11 +2,13 @@ import { doc, onSnapshot } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { db } from "../firebase";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
 
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -21,11 +23,19 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
   console.log(Object.entries(chats));
   return (
     <div className="chats">
       {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
 import Attach from "../img/attach.png";
 import Img from "../img/img.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Input = () => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
   return (
     <div className="input">
       <input type="text" placeholder="Type something..." />
